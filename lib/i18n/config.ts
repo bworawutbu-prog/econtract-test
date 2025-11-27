d010031627bf587b863ea4945c5ea47a2ee95a70
bworawutbu-prog <b.worawut.bu@gmail.com>
2025-11-27 13:35:29 +0700

    Delete lib/i18n directory

--- lib/i18n/config.ts
+++
@@ -1,9 +0,0 @@
-export const locales = ["th", "en"] as const;
-
-export type Locale = (typeof locales)[number];
-
-export const defaultLocale: Locale = "th";
-
-export const isLocale = (value: string | undefined): value is Locale =>
-  !!value && locales.includes(value as Locale);
-
