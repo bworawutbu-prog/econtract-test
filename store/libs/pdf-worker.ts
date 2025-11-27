b1129c4ea44206d5f3dd4ddf4a6724585f6324f3
bworawutbu-prog <b.worawut.bu@gmail.com>
2025-11-27 13:34:32 +0700

    Delete store directory

--- store/libs/pdf-worker.ts
+++
@@ -1,8 +0,0 @@
-"use client";
-
-// Import browser polyfill first
-import './browser-polyfill';
-
-import { pdfjs } from 'react-pdf';
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`; 
--- store/types/BusinessB2BPartnerType.ts
+++
@@ -1,8 +0,0 @@
-export interface BusinessB2BPartner {
-  key: number;
-  has_ca: boolean;
-  tax_id: string;
-  is_verified: boolean;
-  contracts_count: number;
-  partner_name: string;
-}
